6d82dbf993189e397a73e80c464631b9ffacad8e
Bennet Bechtel <bennet.bechtel007@gmail.com>
2024-02-26 15:34:23 +0100

    first commit - commiting all files to the repository

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const blogRoutes = require("./routes/blogRoutes");
+
+// express app
+const app = express();
+
+// connect to mongodb
+const dbURI =
+  "mongodb+srv://blog_page_user:8eBwgJNT60SAWOhd@blogpage.qvrajl1.mongodb.net/BlogData?retryWrites=true&w=majority";
+mongoose
+  .connect(dbURI)
+  .then((result) =>
+    app.listen(5000, function () {
+      console.log(`Connected to db / Server listening on localhost:5000`);
+    })
+  )
+  .catch((err) => console.log(err));
+
+// register view engine
+app.set("view engine", "ejs");
+
+// middleware & static files
+app.use(morgan("dev"));
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+// routes
+app.get("/", (req, res) => {
+  res.redirect("/blogs");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
+});
+
+// blog routes
+app.use("/blogs", blogRoutes)
+
+// 404 page
+app.use((req, res) => {
+  res.render("404", { title: "404" });
+});
